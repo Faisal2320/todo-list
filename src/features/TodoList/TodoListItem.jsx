884711e7f133ff7d87584405ebc4a0282f9fcfdd
Faisal <fsaeed033@gmail.com>
2026-01-04 11:31:02 -0500

    added features and shared diractories and arranged file accordingly

---
+++ src/features/TodoList/TodoListItem.jsx
@@ -0,0 +1,16 @@
+export default function TodoListItem({ onCompleteTodo, task }) {
+  return (
+    <li>
+      <label htmlFor={task.id}>
+        <input
+          id={task.id}
+          type="checkbox"
+          checked={task.isCompleted}
+          onChange={() => onCompleteTodo(task.id)}
+        />
+
+        {task.title}
+      </label>
+    </li>
+  );
+}
