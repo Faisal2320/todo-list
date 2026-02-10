c0964d099afd5a27cccdf809c44d7914bec34808
Faisal <fsaeed033@gmail.com>
2026-02-10 15:34:12 -0500

    removing comments

--- src/pages/TodosPage.jsx
+++ src/pages/TodosPage.jsx
@@ -15,7 +15,6 @@ import {
 } from "../reducers/todoReducer";
 import { useAuth } from "../contexts/AuthContext";
 import loading from "../assets/img/loading.gif";
-import sanitizeInput from "../utils/sanitizeInput";
 
 const TodosPage = () => {
   const [
